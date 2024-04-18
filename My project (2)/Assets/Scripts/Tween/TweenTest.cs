55598769f7aa10d76f2e8354ca8db6ff67115ccc
Iflowoer <kaynireliasylas@gmail.com>
2024-04-18 15:32:39 +0900

    [Feat] DoTween Scene 제작 연출

---
+++ My project (2)/Assets/Scripts/Tween/TweenTest.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;                  //DoTween을 사용하기 위해 추가
+
+
+public class TweenTest : MonoBehaviour
+{
+    Sequence sequence;
+    Tween tween;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //tween=transform.DOMoveX(5, 2);                    //이 오브젝트를 2초동안 X축 5로 이동 시킨다.
+        //transform.DORotate(new Vector3(0, 0, 180), 2);  //이 오브젝트를 2초동안 Z축으로 180도 회전 시킨다.
+        //transform.DOScale(new Vector3(2, 2, 2), 2);         //이 오브젝트를 2초동안 2배로 키운다.
+
+
+        //전체 주석 및 주석 해제 Ctrl + K + C, Ctrl + K + U
+        //Sequence sequence = DOTween.Sequence();               //Tween을 이어서 순서대로 실행 시켜주는 단위.
+        //sequence.Append(transform.DOMoveX(5, 2));
+        //sequence.Append(transform.DORotate(new Vector3(0, 0, 180), 2));
+        //sequence.Append(transform.DOScale(new Vector3(2, 2, 2), 2));
+
+        //transform.DOMoveX(5, 2f).SetEase(Ease.OutBounce);                 //Ease 옵션을 사용하여 바운스 효과 적용
+        //transform.DOShakeRotation(2f, new Vector3(0, 0, 90), 10, 90);     //회전을 z축 90도 강도 10, 랜덤 90으로 힘을 준다.
+
+        //transform.DOMoveX(5, 2f).SetEase(Ease.OutBounce).OnComplete(TweenEnd);  //트윈이 완료되면 TweenEnd함수를 호출.
+
+        sequence = DOTween.Sequence();               //Tween을 이어서 순서대로 실행 시켜주는 단위.
+        sequence.Append(transform.DOMoveX(5, 1));             //Tween 실행
+        sequence.SetLoops(-1, LoopType.Yoyo);                 //Tween 요요형태로 반복 시킨다.
+
+    }
+
+    void TweenEnd()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            sequence.Kill();
+            //tween.Kill();
+        }
+    }
+}
